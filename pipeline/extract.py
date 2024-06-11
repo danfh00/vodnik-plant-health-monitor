90c612a111b3a1b3ff7ec4bfb7c3ef952326490b
laUmar123 <work.umar7@hotmail.com>
2024-06-11 18:46:11 +0100

    script can now populate timezones and country_codes

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -14,11 +14,11 @@ DB_NAME = os.getenv('DB_NAME')
 DB_SCHEMA = os.getenv('DB_SCHEMA')
 
 
-def create_connection(host: str, username: str, password: str, database_name: str, schema: str) -> pymssql.Connection:
+def create_connection(host: str, username: str, password: str, database_name: str) -> pymssql.Connection:
     return pymssql.connect(server=host,
                            user=username,
                            password=password,
-                           database=f"{database_name}.{schema}")
+                           database=database_name)
 
 
 def get_plant_data(plant_id: int) -> dict:
@@ -49,60 +49,84 @@ async def get_all_responses(plant_ids: list[int]) -> list[str]:
     return responses
 
 
-def get_timezone(location_data: list) -> str:
-    """returns timezone from a json response object"""
-    if location_data:
-        return (location_data[-1])
-
-
-def get_country_code(location_data: list) -> str:
-    """returns timezone from a json response object"""
-    if location_data:
-        return (location_data[-2])
-
-
 def get_unique_timezones(responses: list[str]) -> list[tuple]:
     unique_timezones = set()
     for response in responses:
-        location_data = response.get('origin_location')
-        unique_timezones.add(get_timezone(location_data))
+        if 'error' not in response:
+            location_data = response.get('origin_location')
+            unique_timezones.add((location_data[4],))
 
     return list(unique_timezones)
 
 
-def get_unique_countrycodes(responses: list[str]) -> list[tuple]:
-    unique_countrycodes = set()
+def get_unique_country_codes(responses: list[str]) -> list[tuple]:
+    unique_country_codes = set()
     for response in responses:
-        location_data = response.get('origin_location')
-        unique_countrycodes.add(get_timezone(location_data))
+        if 'error' not in response:
+            country_code_data = response.get('origin_location')
+            unique_country_codes.add((country_code_data[3],))
 
-    return list(unique_countrycodes)
+    return list(unique_country_codes)
 
 
-def populate_timezones(timezone_name: list) -> None:
+def populate_timezones(timezone_name: list[tuple], schema: str) -> None:
     """Populates the timezones table"""
     conn = create_connection(DB_HOST, DB_USERNAME,
-                             DB_PASSWORD, DB_NAME, DB_SCHEMA)
+                             DB_PASSWORD, DB_NAME)
     cursor = conn.cursor()
 
-    cursor.execute()
+    try:
+        cursor.executemany(
+            f"INSERT INTO {schema}.timezones VALUES (%s)", timezone_name)
+        conn.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+        conn.rollback()
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def populate_country_codes(country_codes: list[tuple], schema: str) -> None:
+    """Populates the timezones table"""
+    conn = create_connection(DB_HOST, DB_USERNAME,
+                             DB_PASSWORD, DB_NAME)
+    cursor = conn.cursor()
+
+    try:
+        cursor.executemany(
+            f"INSERT INTO {schema}.country_codes VALUES (%s)", country_codes)
+        conn.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+        conn.rollback()
+    finally:
+        cursor.close()
+        conn.close()
 
 
 if __name__ == '__main__':
-    timezones = []
-    for id in range(1, 51):
-        json = get_plant_data(id)
+    plant_ids = range(1, 51)
+    responses = asyncio.run(get_all_responses(plant_ids))
 
-        lat = json.get('origin_location')[0]
-        lon = json.get('origin_location')[1]
-        city = json.get('origin_location')[2]
-        country_code = json.get('origin_location')[3]
-        timezones.append(json.get('origin_location')[4])
+    # timezones = get_unique_timezones(responses)
+    # populate_timezones(timezones, DB_SCHEMA)
+    all_country_codes = get_unique_country_codes(responses)
+    populate_country_codes(all_country_codes, DB_SCHEMA)
 
-        name = json.get('name')
-        scientific_name = json.get('scientific_name')
+    # for id in range(1, 51):
+    #     json = get_plant_data(id)
 
-        reading_at = json.get('recording_taken')
-        moisture = json.get('soil_moisture')
-        temp = json.get('temperature')
-        watered_at = json.get('last_watered')
+    #     lat = json.get('origin_location')[0]
+    #     lon = json.get('origin_location')[1]
+    #     city = json.get('origin_location')[2]
+    #     country_code = json.get('origin_location')[3]
+    #     timezones.append(json.get('origin_location')[4])
+
+    #     name = json.get('name')
+    #     scientific_name = json.get('scientific_name')
+
+    #     reading_at = json.get('recording_taken')
+    #     moisture = json.get('soil_moisture')
+    #     temp = json.get('temperature')
+    #     watered_at = json.get('last_watered')
