3feff79b43187fefca9e4a29755a8556a3100c56
Nabiha <mnabiha09@gmail.com>
2024-06-11 16:01:56 +0100

    Added try and except to the get_plant_data

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -5,13 +5,13 @@ def get_plant_data(plant_id: int) -> dict:
     """Returns plant data in json format from an API using plant ID"""
     try:
         response = requests.get(
-            f"https://data-eng-plants-api.herokuapp.com/plants/{plant_id}")
+            f"https://data-eng-plants-api.herokuapp.com/plants/{plant_id}", timeout=20)
         json = response.json()
         return json
 
-    except Exception as e:
+    except requests.exceptions.Timeout:
         return {"error": "Unable to connect to the API."}
 
 
 if __name__ == '__main__':
-    get_plant_data(7)
+    get_plant_data(77)
