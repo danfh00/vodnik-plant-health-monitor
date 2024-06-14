6b791b155eb9bd3cc4a697d801c563f969a45850
Nabiha <mnabiha09@gmail.com>
2024-06-14 13:15:27 +0100

    updates

--- dashboard/main.py
+++ dashboard/main.py
@@ -31,11 +31,22 @@ def get_locations_data(conn: pymssql.Connection) -> pd.DataFrame:
 
 def get_readings_data(conn: pymssql.Connection) -> pd.DataFrame:
     "Returns locations data from database as a dataframe"
-    query = pd.read_sql("""SELECT ps.common_name, r.reading_at, r.moisture, r.temp, r.watered_at
+    query = pd.read_sql("""SELECT r.plant_id, ps.common_name, r.reading_at, r.moisture, r.temp, r.watered_at
                     FROM gamma.readings AS r
                     JOIN gamma.plants AS p ON r.plant_id = p.plant_id
                     JOIN gamma.plant_species AS ps ON p.species_id = ps.species_id
-                    WHERE reading_at > DATEADD(minute, -30, (SELECT CURRENT_TIMESTAMP));""", conn)
+                    WHERE reading_at > DATEADD(minute, -30, (SELECT CURRENT_TIMESTAMP)) ;""", conn)
+    df = pd.DataFrame(query)
+    return df
+
+
+def get_readings_data_for_specific_plant(conn: pymssql.Connection, common_name: str) -> pd.DataFrame:
+    "Returns locations data from database as a dataframe"
+    query = pd.read_sql(f"""SELECT r.plant_id, ps.common_name, r.reading_at, r.moisture, r.temp, r.watered_at
+                    FROM gamma.readings AS r
+                    JOIN gamma.plants AS p ON r.plant_id = p.plant_id
+                    JOIN gamma.plant_species AS ps ON p.species_id = ps.species_id
+                    WHERE ps.common_name ='{common_name}';""", conn)
     df = pd.DataFrame(query)
     return df
 
@@ -46,11 +57,11 @@ def get_moisture_chart_single_plant(data: pd.DataFrame, plant_choice: str) -> al
     y_max = data['moisture'].max()
     data['reading_at'] = pd.to_datetime(data['reading_at'])
 
-    moisture_data = data[data["plant_id"] == plant_choice]
+    moisture_data = data[data["common_name"] == plant_choice]
     chart = alt.Chart(moisture_data).mark_line().encode(
         x=alt.X('reading_at:T', axis=alt.Axis(title='Time')),
-        y=alt.Y('moisture:Q', scale=alt.Scale(
-            domain=[y_min-1, y_max+1]), axis=alt.Axis(title='Moisture'))
+        y=alt.Y('moisture', scale=alt.Scale(
+            domain=[y_min, y_max]), axis=alt.Axis(title='Moisture'))
     ).properties(
         width=700,
         height=600,
@@ -64,7 +75,7 @@ def get_temperature_chart_single_plant(data: pd.DataFrame, plant_choice) -> alt.
     y_min = data['temp'].min()
     y_max = data['temp'].max()
 
-    temp_data = data[data["plant_id"] == plant_choice]
+    temp_data = data[data["common_name"] == plant_choice]
     chart = alt.Chart(temp_data).mark_line().encode(
         x=alt.X('reading_at:T', axis=alt.Axis(title='Time')),
         y=alt.Y('temp:Q', scale=alt.Scale(
@@ -80,10 +91,10 @@ def get_temperature_chart_single_plant(data: pd.DataFrame, plant_choice) -> alt.
 def get_latest_moisture_chart(latest_data: pd.DataFrame) -> alt.Chart:
     """Creates a bar chart of current moisture readings for all plants"""
     return alt.Chart(latest_data).mark_bar().encode(
-        y=alt.Y('common_name:N', sort='-x', title='Plant name'),
+        y=alt.Y('plant_id:N', sort='-x', title='Plant ID'),
         x=alt.X('moisture:Q', title='Soil Moisture'),
         color=alt.Color("moisture", scale=alt.Scale(
-            scheme="blues"))
+            scheme="redblue"))
     ).properties(
         width=700,
         height=600,
@@ -93,7 +104,7 @@ def get_latest_moisture_chart(latest_data: pd.DataFrame) -> alt.Chart:
 def get_latest_temperature_chart(latest_data: pd.DataFrame) -> alt.Chart:
     """Creates a bar chart of current temperature readings for all plants"""
     return alt.Chart(latest_data).mark_bar().encode(
-        y=alt.Y('common_name:N', sort='-x', title='Plant name'),
+        y=alt.Y('plant_id:N', sort='-x', title='Plant ID'),
         x=alt.X('temp:Q', title='Temperature'),
         color=alt.Color("temp", scale=alt.Scale(
             scheme="orangered"))
@@ -101,6 +112,41 @@ def get_latest_temperature_chart(latest_data: pd.DataFrame) -> alt.Chart:
         width=700,
         height=600,
     ).interactive()
+
+
+def get_average_moisture_chart(data: pd.DataFrame) -> alt.Chart:
+    """Creates a line graph of moisture over time for a given plant id"""
+    y_min = data['moisture'].min()
+    y_max = data['moisture'].max()
+    data['reading_at'] = pd.to_datetime(data['reading_at'])
+
+    chart = alt.Chart(data).mark_line().encode(
+        x=alt.X('reading_at:T', axis=alt.Axis(title='Time')),
+        y=alt.Y('moisture:Q', scale=alt.Scale(
+            domain=[y_min-1, y_max+1]), axis=alt.Axis(title='Moisture'))
+    ).properties(
+        width=700,
+        height=600,
+    ).interactive()
+
+    return chart
+
+
+def get_average_temperature_chart(data: pd.DataFrame) -> alt.Chart:
+    """Creates a line graph of temperature over time for a given plant id"""
+    y_min = data['temp'].min()
+    y_max = data['temp'].max()
+
+    chart = alt.Chart(data).mark_line().encode(
+        x=alt.X('reading_at:T', axis=alt.Axis(title='Time')),
+        y=alt.Y('temp:Q', scale=alt.Scale(
+            domain=[y_min, y_max]), axis=alt.Axis(title='Temperature'))
+    ).properties(
+        width=700,
+        height=600,
+    ).interactive()
+
+    return chart
 
 
 def build_dashboard():
@@ -118,17 +164,14 @@ def build_dashboard():
     with tab_historical:
         # Uses data from the s3 bucket. Currently using data from database
         st.write("Plant")
-        plant_ids = historical_data['plant_id'].unique().tolist()
-        plant_option = st.selectbox("Choose a plant", plant_ids)
+        st.header(f'🌡️ Temperature Readings 🌡️')
+        st.write(get_average_temperature_chart(
+            historical_data))
 
-        st.header(f'🌡️ Temperature Readings for Plant {plant_option}🌡️')
-        st.write(get_temperature_chart_single_plant(
-            historical_data, plant_choice=plant_option))
+        st.header(f'💧 Soil Moisture Readings 💧')
 
-        st.header(f'💧 Soil Moisture Readings for Plant{plant_option}💧')
-
-        st.write(get_moisture_chart_single_plant(
-            historical_data, plant_choice=plant_option))
+        st.write(get_average_moisture_chart(
+            historical_data))
 
     with tab_location:
         # Location Map
@@ -143,6 +186,22 @@ def build_dashboard():
 
         st.header('Latest Temperature Readings 🌡️')
         st.write(get_latest_temperature_chart(readings_df))
+
+    #  Plant name filter
+        st.write("Plant Filter")
+        plant_names = readings_df['common_name'].unique().tolist()
+        plant_option = st.selectbox("Choose a plant", plant_names)
+
+        plant_readings = get_readings_data_for_specific_plant(
+            conn, plant_option)
+
+        st.subheader(f'Temperature Readings for {plant_option.title()}🌡️')
+        st.write(get_temperature_chart_single_plant(
+            plant_readings, plant_choice=plant_option))
+
+        st.subheader(f' Soil Moisture Readings for {plant_option.title()}💧')
+        st.write(get_moisture_chart_single_plant(
+            plant_readings, plant_choice=plant_option))
 
 
 if __name__ == '__main__':
